b061de97959bd56ad617662fedb83949b2d5b9ca
Kitravee Siwatkittisuk <32170386+Kitravee@users.noreply.github.com>
2018-06-08 04:03:15 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,50 +1,53 @@
-<?php // callback.php
-
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-require("line.php");
-$access_token = 'FJVHsIVISrTjsYR87I5h3PySe9Nk3p8PklGovZcaEard3VxglFqCNj1VKSPaOr0aY6A5gkZRENuCjVgeBhRMuWDfQLy8Cx/1NikOnZMPOg8C9qp1hmJy/A0ajfKhf+cRb+KAsH09um2womP/YTt6VwdB04t89/1O/w1cDnyilFU=';
-
+<?php
+ require("line.php");
+//require("add_device.php");
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
+
 if (!is_null($events['events'])) {
-	// Loop through each event
-	if (!is_null($events['events'])) {
-	  echo "line bot";
-	  foreach ($events['events'] as $event) {
-	    if ($event['type'] == 'follow') {
-	      $userId = $event['source']['userId'];
-	      send_Menu($userId);
-	    }
-	    else if ($event['type'] == 'postback') {
-	      $userId = $event['source']['userId'];
-	      $data = $event['postback']['data'];
-	      send_LINE($data,$userId);
-	    }
-	    else if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-	      // Get text sent
-	      $text = $event['message']['text'];
-	      // Get replyToken
-	      //$replyToken = $event['replyToken'];
-	      // Build message to reply back
-	      $userId = $event['source']['userId'];
-	      $pos = strpos($text, ":");
-		  if($pos){
-
-		    $splitMsg = explode(":", $text);
-		    $topic = $splitMsg[0];
-		    $msg = $splitMsg[1];
-
-
-		   if($topic == "Acknowledge" ){
-		    $text = "ACK";
-		    check_userid($userId,$text,$msg);
-		    //getMqttfromlineMSG($text);  
-		   }
-		 }else{    
+  echo "line bot";
+  // Loop through each event
+  foreach ($events['events'] as $event) {
+    // Reply only when message sent is in 'text' format
+    if ($event['type'] == 'follow') {
+      $userId = $event['source']['userId'];
+      //send_FALL($userId);
+      send_Menu($userId);
+      //send_LOWBAT($userId);
+      //send_PRESS($userId);
+      //send_LINE('json','Ue77a191627f6ac91899e75d92264310c');
+    }
+    else if ($event['type'] == 'postback') {
+      $userId = $event['source']['userId'];
+      $data = $event['postback']['data'];
+      send_LINE($data,$userId);
+    }
+    else if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+      // Get text sent
+      $text = $event['message']['text'];
+      // Get replyToken
+      //$replyToken = $event['replyToken'];
+      // Build message to reply back
+      $userId = $event['source']['userId'];
+      $pos = strpos($text, ":");
+          if($pos){
+           
+            $splitMsg = explode(":", $text);
+            $topic = $splitMsg[0];
+            $msg = $splitMsg[1];
+            
+            
+           if($topic == "Acknowledge" ){
+            $text = "ACK";
+            check_userid($userId,$text,$msg);
+            //getMqttfromlineMSG($text);  
+           }
+           
+             
+          }else{    
            
           if($text == "Check" || $text == "CHECK" || $text == "check" || $text == "เช็ค" || $text == "เช็คอุปกรณ์"){
             $text = "CHECK";
@@ -69,5 +72,13 @@ if (!is_null($events['events'])) {
             //send_LINE('Incorrect command: type "Check" for check status device , "login:<device name>" for login , "logout:<device name>" for logout',$userId);
           }
           }
+          
+      
+      
+    
+      
+    }
+  }
 }
 echo "OK";
+?>
